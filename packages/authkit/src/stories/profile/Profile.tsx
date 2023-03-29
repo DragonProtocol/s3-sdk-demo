831fbdf73ebc868b99fe505d756b31bc53856bc0
ttang <ttang@ttang-MacBook-Pro-2021.local>
2023-03-29 16:37:17 +0800

    feat: profile stories

--- packages/authkit/src/stories/profile/Profile.tsx
+++ packages/authkit/src/stories/profile/Profile.tsx
@@ -5,7 +5,7 @@ import App from "../app/App";
 const Page: React.VFC = () => {
   return (
     <App>
-      <Profile did="did:pkh:eip155:137:0x4630cf0fa55f83e11e43286ff04fc6930e1eb095" />
+      <Profile did="did:pkh:eip155:1:0x4630cf0fa55f83e11e43286ff04fc6930e1eb095" />
     </App>
   );
 };
