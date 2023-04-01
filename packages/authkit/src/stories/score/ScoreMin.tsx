8834eeefed75452b0fe5855a3a6b0f51d2e26e79
Hulk <33158585+byteHulk@users.noreply.github.com>
2023-04-01 10:44:59 +0800

    fix: fix scoreMin bug

--- packages/authkit/src/stories/score/ScoreMin.tsx
+++ packages/authkit/src/stories/score/ScoreMin.tsx
@@ -5,7 +5,7 @@ import App from '../app/App'
 const Page: React.VFC = () => {
   return (
     <App>
-      <ScoreMin threadId="kjzl6kcym7w8y7dy1njfhc8ewv822i1qw1ty0o0vy5a4w5yu5cuqwqnbfcuh0qx" />
+      <ScoreMin threadId="kjzl6kcym7w8y9rxuti82hd71oxo3z36c9qe2stn3r12a2mh7bd2minvk2x6xqr" />
     </App>
   )
 }
