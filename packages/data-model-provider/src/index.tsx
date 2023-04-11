5a8c9e640408333cdcec96af98b497d81e1b8d8c
ttang <ttang@ttang-MacBook-Pro-2021.local>
2023-04-11 13:43:55 +0800

    feat: profile datamodel & provider

---
+++ packages/data-model-provider/src/index.tsx
@@ -0,0 +1,49 @@
+import React, { createContext, useCallback, useContext } from "react";
+import { DIDSession } from "did-session";
+import { Model } from "./Model";
+
+export { Model } from "./Model";
+
+const ModelsContext =
+  createContext<{
+    authModelWithSess: (sess: DIDSession) => void;
+    disconnectModelFromSess: () => void;
+  } | null>(null);
+
+export default function DataModelProvider({
+  models,
+  children,
+}: React.PropsWithChildren & { models: Model[] }) {
+  const authModelWithSess = useCallback(
+    (sess: DIDSession) => {
+      models.forEach((item) => {
+        item.authComposeClient(sess);
+      });
+    },
+    [models]
+  );
+  const disconnectModelFromSess = useCallback(() => {
+    models.forEach((item) => {
+      item.resetComposeClient();
+    });
+  }, [models]);
+
+  return (
+    <ModelsContext.Provider
+      value={{
+        authModelWithSess,
+        disconnectModelFromSess,
+      }}
+    >
+      {children}
+    </ModelsContext.Provider>
+  );
+}
+
+export const useS3ModelContext = () => {
+  const modelCtx = useContext(ModelsContext);
+  if (!modelCtx) {
+    throw new Error("init data-model-provider first");
+  }
+  return modelCtx;
+};
